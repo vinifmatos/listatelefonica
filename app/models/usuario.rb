c405025c19e07ae84d5cbd4183ec064330ef84a2
outcoin <vinicius.freire4@gmail.com>
2018-11-05 14:26:54 -0200

    adicionado validação no login do usuario

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -2,7 +2,7 @@ class Usuario < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :recoverable, :registerable, :trackable and :omniauthable
   devise :database_authenticatable, :validatable, :rememberable
-  validates :login, uniqueness: true
+  validates :login, uniqueness: true, presence: true
   enum acesso: [:admin, :cadastro, :consulta]
 
   protected
---
+++ db/migrate/20181105161011_add_constraint_valida_login_to_usuario.rb
@@ -0,0 +1,16 @@
+class AddConstraintValidaLoginToUsuario < ActiveRecord::Migration[5.2]
+  def up
+    execute <<-SQL
+      ALTER TABLE usuarios
+        ADD CONSTRAINT valida_login
+          CHECK (char_length(login) >= 1);
+    SQL
+  end
+
+  def down
+    execute <<-SQL
+      ALTER TABLE usuarios
+        DROP CONSTRAINT valida_login
+    SQL
+  end
+end
