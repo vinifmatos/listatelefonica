f6d52a86999ccce8c57bda94f7ff411b4fde1e6c
outcoin <vinicius.freire4@gmail.com>
2018-11-05 17:01:20 -0200

    adicionado js para notificação de erros do model

--- app/models/contato.rb
+++ app/models/contato.rb
@@ -9,23 +9,26 @@ class Contato < ApplicationRecord
   private
 
   def numero_informado
-    errors.add(:base, 'Telefone ou celular de ser informado!') if telefone.blank? && celular.blank?
+    if telefone.blank? && celular.blank?
+      errors.add(:telefone, 'não pode ficar em branco.')
+      errors.add(:celular, 'não pode ficar em branco.')
+    end
   end
 
   def valida_telefone
-    unless telefone.nil?
-      errors.add(:telefone, 'Telefone inválido') unless telefone.gsub(/[()\s-]/, '') =~ /(^\d{8}\z|^\d{10}\z)/
+    unless telefone.present?
+      errors.add(:telefone, 'está em formato inválido') unless telefone.gsub(/[()\s-]/, '') =~ /(^\d{8}\z|^\d{10}\z)/
     end
   end
 
   def valida_celular
-    unless celular.nil?
-      errors.add(:celular, 'Celular inválido') unless celular.gsub(/[()\s-]/, '') =~ /(^\d{9}\z|^\d{11}\z)/
+    unless celular.present?
+      errors.add(:celular, 'está em formato inválido') unless celular.gsub(/[()\s-]/, '') =~ /(^\d{9}\z|^\d{11}\z)/
     end
   end
 
   def remover_caracteres
-    telefone.gsub!(/[()\s-]/, '') unless telefone.nil?
-    celular.gsub!(/[()\s-]/, '') unless celular.nil?
+    telefone&.gsub!(/[()\s-]/, '')
+    celular&.gsub!(/[()\s-]/, '')
   end
 end
